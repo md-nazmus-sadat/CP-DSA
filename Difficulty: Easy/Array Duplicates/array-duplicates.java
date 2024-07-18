2c9a3e0b587018f240ee611549c84e3b0ad7e5db
Md Nazmus Sadat <109305457+md-nazmus-sadat@users.noreply.github.com>
2024-07-18 20:52:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.duplicates(arr);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> duplicates(int[] arr) {
+        ArrayList<Integer> list=new ArrayList<>();
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i + 1;j<n;j++)
+            {
+                if(arr[i] == arr[j])
+                {
+                    list.add(i);
+                }
+            }
+        }
+        if(list.size() > 0) {
+            Collections.sort(list);
+            return list;
+        }
+        else {
+            list.add(-1);
+            return list;
+        }
+    }
+}
