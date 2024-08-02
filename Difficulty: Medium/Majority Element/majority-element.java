37d2967caac52598a289f381b333e62afad1b75d
Md Nazmus Sadat <109305457+md-nazmus-sadat@users.noreply.github.com>
2024-08-02 20:30:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+        // your code here
+        int n = size;
+        int cnt = 0;
+        int el = 0;
+        for ( int i = 0 ; i < size; i++)
+        {
+            if ( cnt == 0)
+            {
+                cnt = 1;
+                el = a[i];
+            }
+            else if ( el == a[i])
+            {
+                cnt++;
+            }
+            else cnt--;
+            
+        }
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
+        
+        
+    }
+}
